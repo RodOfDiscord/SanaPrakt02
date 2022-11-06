a5ff0d2ea3fa5db6e85b359f8fe96b2e5dbbc50f
Rod_Of_Harmony <ipz222_pis@student.ztu.edu.ua>
2022-11-06 20:23:51 +0200

    last 2 lines edited

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("text");
 Console.WriteLine("text");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Sana Commerce Software Development Bootcamp");
