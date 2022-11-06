147d0980c2fd88d2da47781f02efe6ab95da96b1
Rod_Of_Harmony <ipz222_pis@student.ztu.edu.ua>
2022-11-06 20:22:45 +0200

    first 3 lines edited

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("text");
+Console.WriteLine("text");
+Console.WriteLine("text");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
